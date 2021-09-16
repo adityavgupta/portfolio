10e444371d5f51c8fe2d6c69c46bdc0eb7e45fdc
Aditya Gupta <aditya@0587425422.wireless.umich.net>
2021-09-16 16:59:10 -0400

    corrected pdf view with iframe

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,18 +1,22 @@
 import React,  {Component} from 'react';
-
-import SinglePagePDFViewer from "../components/pdf/single_page";
+import "../App.css";
+// import SinglePagePDFViewer from "../components/pdf/single_page";
 // import AllPagesPDFViewer from "../components/pdf/all_pages";
 
 /* This is required only if the project file is located 
 inside the app. Otherwise you can use the external link of the pdf file*/
-import samplePDF from "../pdf/resume.pdf";
+// import samplePDF from "../pdf/resume_latest.pdf";
 
 
 class Resume extends Component {
   render(){
     return (
-        <div className="Resume">
-          <SinglePagePDFViewer pdf={samplePDF} />
+        <div className="Resume" align="center">
+          <iframe title="res" src="https://drive.google.com/file/d/1hwqltaIk7L4j1ZnFQePhiPN9YipWuBHW/preview" 
+          width="640" 
+          height="680" 
+          allow="autoplay"
+          ></iframe>
           
         </div>
       );
