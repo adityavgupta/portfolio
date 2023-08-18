83a5447c719a0943f02b19496256936f479dcaf0
Aditya Vikram Gupta <36277988+adityavgupta@users.noreply.github.com>
2023-08-18 11:07:09 -0700

    Update Education.js

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -7,7 +7,7 @@ class Education extends Component {
             <div className="condiv">
                 <h1 className="subtopic">My Education</h1>
                 <hr/>
-                <Widecard name="umich" title="M.S. Signal and Image Processing and Machine Learning (EECS)" where="University of Michigan Ann-Arbor" from="August 2021" to="Present" />
+                <Widecard name="umich" title="M.S. Signal and Image Processing and Machine Learning (EECS)" where="University of Michigan Ann-Arbor" from="August 2021" to="April 2023" />
                 <Widecard name="uiuc" title="B.S. Computer Engineering" where="University of Illinois at Urbana-Champaign" from="August 2017" to="May 2021"/>
             </div>
             
